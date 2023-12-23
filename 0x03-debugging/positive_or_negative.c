bfb3878b039bad372bdae0a5ab39a5027abb083c
Raheem <rah4smile2006@gmail.com>
2023-12-22 22:39:14 -0800

    copy this mai file

---
+++ 0x03-debugging/positive_or_negative.c
@@ -0,0 +1,16 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+/* more headers goes there */
+
+/* betty style doc for function main goes there */
+void positive_or_negative(int i)
+{
+	if (i > 0);
+		print("%d is positive\n", i);
+	else if (i < 0)
+		print("%d is negative\n", i);
+	else
+		print("%d is zero\n", i);
+
+}
